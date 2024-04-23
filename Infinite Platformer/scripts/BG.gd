edeaaff332bdd3679f15c2d404359d1048e08048
EricBL3 <ericbuitron3@gmail.com>
2024-04-23 10:04:57 -0600

    Fix parallax bg

--- Infinite Platformer/scripts/BG.gd
+++ Infinite Platformer/scripts/BG.gd
@@ -1,13 +1,11 @@
 extends ParallaxBackground
-@onready var sprite = $ParallaxLayer/Sprite2D
-@export var scroll_speed = 15
+@onready var texture_rect = $ParallaxLayer/TextureRect
+@export var scroll_speed = 50
 @export var bg_texture: CompressedTexture2D = preload("res://assets/textures/bg/Blue.png")
 
 func _ready():
-	sprite.texture = bg_texture
+	texture_rect.texture = bg_texture
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	sprite.region_rect.position += delta * Vector2(scroll_speed, scroll_speed)
-	if sprite.region_rect.position >= Vector2(64, 64):
-		sprite.region_rect.position = Vector2.ZERO
+	scroll_offset.x -= scroll_speed * delta
