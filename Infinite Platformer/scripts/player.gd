90b8c5e49dd59e62ce9792314461fda6aa5214f7
EricBL3 <ericbuitron3@gmail.com>
2024-04-23 10:09:03 -0600

    Add player jump

---
+++ Infinite Platformer/scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var jump_velocity = 400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y -= jump_velocity
+
+	move_and_slide()
