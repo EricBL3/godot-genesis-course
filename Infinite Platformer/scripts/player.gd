628d2aa1f90b4ff8931d9d34e8b097238a675ed8
EricBL3 <ericbuitron3@gmail.com>
2024-04-25 08:49:08 -0600

    Implement player animations

--- Infinite Platformer/scripts/player.gd
+++ Infinite Platformer/scripts/player.gd
@@ -2,10 +2,19 @@ extends CharacterBody2D
 class_name Player
 
 @export var jump_velocity = 400.0
+@onready var animated_sprite = $AnimatedSprite2D
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _process(delta):
+	if is_on_floor():
+		if animated_sprite.animation != "run":
+			animated_sprite.play("run")
+	else:
+		if animated_sprite.animation != "jump":
+			animated_sprite.play("jump")
 
 func _physics_process(delta):
 	# Add the gravity.
